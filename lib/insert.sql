64c49483394f690015e983bfc60e9dbdc154a5c9
Latoshia Williams <lswillms@yahoo.com>
2023-01-29 16:38:00 -0600

    commit tree e4e84b898bdaa6fc0322f7f38f6f0b8559977655

---
+++ lib/insert.sql
@@ -0,0 +1,27 @@
+INSERT INTO bears(name, age, sex, color, temperament,alive)
+VALUES ("Mr.Chocolate", 10, "M", "brown", "gentle", true);
+
+INSERT INTO bears(name, age, sex, color, temperament,alive)
+VALUES ("Rowdy", 1, "M", "white", "rowdy", true);
+
+INSERT INTO bears(name, age, sex, color, temperament,alive)
+VALUES ("Tabitha", 11, "F", "black", "gentle", true);
+
+INSERT INTO bears(name, age, sex, color, temperament,alive)
+VALUES ("Sergeant Brown", 44, "F", "gray", "stern", true);
+
+INSERT INTO bears(name, age, sex, color, temperament,alive)
+VALUES ("Melissa", 13, "F", "brown", "quiet", true);
+
+INSERT INTO bears(name, age, sex, color, temperament,alive)
+VALUES ("Grinch", 10, "M", "brown", "gentle", true);
+
+INSERT INTO bears(name, age, sex, color, temperament,alive)
+VALUES ("Wendy", 35, "F", "white", "lazy", true);
+
+INSERT INTO bears(name, age, sex, color, temperament,alive)
+VALUES (NULL, 54, "M", "brown", "loud", true);
+
+
+
+
